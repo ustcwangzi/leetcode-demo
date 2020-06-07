03b8099c01b623f5b69d5128b7659ca6ac3a0aff
ustcwangzi <ustcwangzi@foxmail.com>
2020-06-07 12:21:52 +0800

    MajorityElement

---
+++ src/main/java/com/wz/lists/MajorityElementII.java
@@ -0,0 +1,22 @@
+package com.wz.lists;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MajorityElementII {
+    public static void main(String[] args) {
+        int[] nums = new int[]{3, 2, 3};
+        System.out.println(majorityElement(nums));
+
+        nums = new int[]{1, 1, 1, 3, 3, 2, 2, 2};
+        System.out.println(majorityElement(nums));
+    }
+
+    public static List<Integer> majorityElement(int[] nums) {
+        List<Integer> result = new LinkedList<>();
+
+        return result;
+    }
+}
