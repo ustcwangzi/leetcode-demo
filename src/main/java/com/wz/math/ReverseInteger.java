8d3b180b33bbdf18bd264b62e96b63bc4e01158e
ustcwangzi <ustcwangzi@foxmail.com>
2020-08-24 08:00:35 +0800

    ReverseInteger

--- src/main/java/com/wz/math/ReverseInteger.java
+++ src/main/java/com/wz/math/ReverseInteger.java
@@ -12,7 +12,8 @@ package com.wz.math;
  * Output: -321
  *
  * Note:
- * Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+ * Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1].
+ * For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
  */
 public class ReverseInteger {
     public static void main(String[] args) {
