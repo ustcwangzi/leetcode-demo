35586a04bc719c1f4f8176f26c77b1198765a427
wangzi <ustcwangzi@foxmail.com>
2021-03-02 08:58:51 +0800

    LongestHappyString

--- src/main/java/com/wz/dynamicprogramming/LongestHappyString.java
+++ src/main/java/com/wz/dynamicprogramming/LongestHappyString.java
@@ -1,5 +1,7 @@
 package com.wz.dynamicprogramming;
 
+import java.util.PriorityQueue;
+
 /**
  * A string is called happy if it does not have any of the strings 'aaa', 'bbb' or 'ccc' as a substring.
  * Given three integers a, b and c, return any string s, which satisfies following conditions:
@@ -32,29 +34,46 @@ public class LongestHappyString {
     }
 
     /**
-     * 因为只有三个字符，可以用 prev、prevPrev 记录前两次使用的字段索引，然后逐次将剩余可用次数最多的字符添加到结果中
+     * 大根堆
+     * 优先选择剩余次数最多的元素，因为不允许一个元素连续出现三次，因此需要在加入结果集之前判断前两个元素是否与当前元素相等
+     * 若相等，则选择出现次数次多的元素加入结果集，然后再将出现次数最多的继续加入结果集
+     * 注意，每次加入结果集之后，如果剩余的次数大于 0，则需要将元素重新放入优先级队列中
      */
     public static String longestDiverseString(int a, int b, int c) {
-        int[] counts = new int[]{a, b, c};
-        StringBuilder sb = new StringBuilder();
-        int prev = -1, prevPrev = -1;
-        while (true) {
-            int max = 0, index = -1;
-            for (int i = 0; i < 3; i++) {
-                // 找到剩余可用次数最多，并且和前两次不一样的字符索引
-                if (counts[i] > max && prev != i && prevPrev != i) {
-                    max = counts[i];
-                    index = i;
+        PriorityQueue<int[]> queue = new PriorityQueue<>((o1, o2) -> Integer.compare(o2[0], o1[0]));
+        if (a > 0) {
+            queue.offer(new int[]{a, 0});
+        }
+        if (b > 0) {
+            queue.offer(new int[]{b, 1});
+        }
+        if (c > 0) {
+            queue.offer(new int[]{c, 2});
+        }
+
+        StringBuilder builder = new StringBuilder();
+        while (!queue.isEmpty()) {
+            int[] first = queue.poll();
+            int size = builder.length();
+            // 已连续出现两个相同元素，不能选择当前元素
+            if (size >= 2 && builder.charAt(size - 1) == first[1] + 'a' && builder.charAt(size - 2) == first[1] + 'a') {
+                if (queue.isEmpty()) {
+                    break;
+                }
+
+                int[] second = queue.poll();
+                builder.append((char) ('a' + second[1]));
+                // 重新加入堆中
+                if (--second[0] > 0) {
+                    queue.offer(second);
                 }
             }
-            if (max == 0) {
-                break;
+
+            builder.append((char) ('a' + first[1]));
+            if (--first[0] > 0) {
+                queue.offer(first);
             }
-            sb.append((char) ('a' + index));
-            --counts[index];
-            prevPrev = prev;
-            prev = index;
         }
-        return sb.toString();
+        return builder.toString();
     }
 }
