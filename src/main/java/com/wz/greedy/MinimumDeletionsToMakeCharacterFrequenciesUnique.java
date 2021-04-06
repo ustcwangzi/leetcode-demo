0e77615f93e58c534dcfcd9c673db2ae11c95215
wangzi <ustcwangzi@foxmail.com>
2021-04-06 09:43:42 +0800

    CheckArrayFormationThroughConcatenation

--- src/main/java/com/wz/greedy/MinimumDeletionsToMakeCharacterFrequenciesUnique.java
+++ src/main/java/com/wz/greedy/MinimumDeletionsToMakeCharacterFrequenciesUnique.java
@@ -32,7 +32,7 @@ public class MinimumDeletionsToMakeCharacterFrequenciesUnique {
 
     /**
      * 首先统计各个字母的出现次数存入数组 frequency，然后遍历数组，如果出现次数为 0，直接跳过
-     * 否则使用 HashSet 进行去重，如果加进来的数目已存在，就进行自减，减到 HashSet 中没有的数目
+     * 否则使用 HashSet 记录出现次数，如果加进来的数目已存在，就进行自减，减到 HashSet 中没有的数目
      */
     public static int minDeletions(String s) {
         char[] array = s.toCharArray();
